36cf65ecedbe838272e4ccda920b8e3b1ce33876
Daniel Vieira <danielvieiraufc@gmail.com>
2013-09-11 09:23:34 +0000

    index & new // lancamentos

--- app/models/lancamento.rb
+++ app/models/lancamento.rb
@@ -3,6 +3,6 @@ class Lancamento < ActiveRecord::Base
 	belongs_to :categoria
 	belongs_to :tipo_lancamento
 
-    validates_presence_of :valor, :data_pagamento, :user_id, :tipo_lancamento_id, :categoria_id, :descricao
+    validates_presence_of :valor, :user_id, :tipo_lancamento_id, :categoria_id, :descricao
     default_scope { order(:id => :asc) }
 end
