7a8a87d30e09b17c692b623c4d9ca29020c7f28a
Danilo J <danilo@josino.com.br>
2013-09-17 19:08:59 -0300

    correcoes

--- app/controllers/lancamentos_controller.rb
+++ app/controllers/lancamentos_controller.rb
@@ -29,7 +29,7 @@ class LancamentosController < ApplicationController
     @lancamento.user = current_user
     respond_to do |format|
       if @lancamento.save
-        format.html { redirect_to @lancamento, notice: 'Lancamento was successfully created.' }
+        format.html { redirect_to lancamentos_path, notice: 'Lancamento was successfully created.' }
         format.json { render action: 'show', status: :created, location: @lancamento }
       else
         format.html { render action: 'new' }
@@ -44,7 +44,7 @@ class LancamentosController < ApplicationController
     @lancamento.user = current_user
     respond_to do |format|
       if @lancamento.update(lancamento_params)
-        format.html { redirect_to @lancamento, notice: 'Lancamento was successfully updated.' }
+        format.html { redirect_to lancamentos_path, notice: 'Lancamento was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
